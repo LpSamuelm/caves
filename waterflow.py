c436c07c1c7751d60111970c31b55cde9b6f74a5
LpSamuelm <powpowd@gmail.com>
2013-05-08 19:12:38 +0200

    Switched loop to one that actually works

--- waterflow.py
+++ waterflow.py
@@ -1,12 +1,29 @@
-infile = open('simple_cave.txt', 'r')
+from sys import argv
+
+argfile = False
+
+try:
+    argfile = argv[1]
+except IndexError:
+    af = raw_input("What file do you want to use for simulation? >")
+while not argfile:
+    try:
+        infile = open(af, 'r')
+        argfile = af
+    except IOError:
+        af = raw_input("That is not a valid file.\nWhat file do you want to use for simulation? >")
+    
 cave = infile.read()
 infile.close()
 cave = cave.split('\n')
 watertiles = int(cave[0])
+
 cave = cave[2:-1]
 cave = [list(x) for x in cave]
-for boob in cave:
-    print boob
+
+for row in cave: ## Delete later
+    print row
+    
 cavecols = []
 for t in range(0, len(cave[0])):
     cavecols.append(''.join([x[t] for x in cave]))
@@ -24,12 +41,14 @@ def checkWater(x, y):
             return x, y + 1
         elif cave[y][x + 1] == ' ':
             return x + 1, y
-        for ypos in cave[y - 1::-1]:
-            if ' ' in ypos:
-                for xpos in cave[ypos][::-1]:
-                    if cave[ypos][xpos - 1] == ' ' and xpos == ' ':
-                        return xpos, ypos
-                return False
+        ypos = y
+        xpos = len(cavecols)
+        for row in cave[y - 1::-1]:
+            ypos -= 1
+            for column in cavecols[::-1]:
+                xpos -= 1
+                if column[ypos] == ' ' and row[xpos - 1] == '~':
+                    return xpos, ypos
     except IndexError:
         return False
 
@@ -48,4 +67,26 @@ def flow(numflows, pos=startpos):
             return
 
 flow(watertiles)
-print '\n'.join([''.join(x) for x in cave])
+cavecols = []
+for t in range(0, len(cave[0])):
+    cavecols.append(''.join([x[t] for x in cave]))
+
+def measureDepth(cols):
+    depths = []
+    for col in cols:
+        coldepth = 0
+        for symbol in col:
+            if symbol == '~':
+                coldepth += 1
+            elif symbol == ' ' and coldepth > 0:
+                coldepth = '~'
+                break
+        depths.append(coldepth)
+    return depths
+
+cavestring = '\n'.join([''.join(x) for x in cave])
+depthstring = ' '.join([str(x) for x in measureDepth(cavecols)])
+
+outfile = open('output.txt', 'w')
+outfile.write(cavestring + '\n' + depthstring)
+outfile.close()
