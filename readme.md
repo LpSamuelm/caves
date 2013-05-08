842897c4927ef9d098d28f5c64ffa465efd4d95b
LpSamuelm <powpowd@gmail.com>
2013-05-08 20:52:26 +0200

    Added an excercise optimized version, new format
    Now outputs two files, and there is a slightly faster version which only
    works with complex_cave.txt. Updated readme to reflect these changes.

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 ##What it does
 `waterflow.py` simulates water flowing (only from left to right) in a cave, and was made for an excercise on Puzzlenode - see [here](http://www.puzzlenode.com/puzzles/11-hitting-rock-bottom).
 
+`waterflow-excerciseonly.py` does the same thing, except optimized for the excercise and not as flexible.
+
 ##How to use
 To launch, either run the script with a file and an output file as the arguments, or type in your filename choices at the prompt that appears at the start.
 
@@ -8,4 +10,6 @@ Supply a file in the format seen in `simplecave.txt`. The first line should cont
 
 You can put the water starting point anywhere, but it is advised to put it along the left edge, seeing as the water will never flow to the left.
 
-The output file will contain the cave as it would look with your specified amount of water in it, and a list  separated by spaces containing the depths of every column. A `~` here represents a column that has empty space under the water, and therefore not a measurable depth.
+The output file will contain the cave as it would look with your specified amount of water in it. A `~` here represents a column that has empty space under the water, and therefore not a measurable depth.
+
+An output file with the suffix `-depths` will also be generated. This is a list separated by spaces containing the depths of every column.
--- waterflow-excerciseonly.py
+++ waterflow-excerciseonly.py
@@ -1,19 +1,4 @@
-from sys import argv ## Needed for command line arguments
-
-argfile = False ## Dummy variable
-
-try: ## This whole clause is to set the input file
-    argfile = argv[1]
-except IndexError:
-    af = raw_input("What file do you want to use for simulation? >")
-while not argfile:
-    try:
-        infile = open(af, 'r')
-        argfile = af
-    except IOError:
-        af = raw_input("That is not a valid file.\nWhat file do you want to use for simulation? >")
- 
-
+infile = open('complex_cave.txt')
 cave = infile.read()
 infile.close()
 cave = cave.split('\n') ## Splits input file into list of rows
@@ -28,8 +13,6 @@ for t in range(0, len(cave[0])): ## Sets cavecols to a list of the columns, left
 for xpos, x in enumerate(cavecols): ## Sets the start position of water
     if '~' in x:
         startpos = [xpos, cavecols[xpos].index('~')]
-if not startpos:
-    print "Invalid file."
 
 def putWater(x, y):
     """Puts a water tile on cave[y][x]"""
@@ -82,32 +65,8 @@ def measureDepth(cols):
         depths.append(coldepth) ## Returns a list of depths, from left to right
     return depths
 
-cavestring = '\n'.join([''.join(x) for x in cave]) ## Write-friendly version of cave
 depthstring = ' '.join([str(x) for x in measureDepth(cavecols)]) ## Write-friendly version of depths
 
-outfile = False ## Dummy variable
-
-try: ## This whole clause is to set the output file
-    outfile = argv[2]
-except IndexError:
-    of = raw_input("What file do you want to output to? >")
-while not outfile:
-    while not of.strip():
-        of = raw_input("That is not a valid file.\nWhat file do you want to output to? >")
-    try:
-        output = open(of, 'w')
-        outfile = of
-    except IOError:
-        of = raw_input("That is not a valid file.\nWhat file do you want to output to? >")
-
-try: ## Sets parts of the filename for depths file generation later
-    outname, outextension = outfile[:outfile.index('.')], outfile[outfile.index('.'):]
-except ValueError:
-    pass
-
-depthsfile = open(outname + '-depths' + outextension, 'w') ## Opens depths file
-output.write(cavestring) ## Writes output
-depthsfile.write(depthstring) ## Writes depths
-
+output = open('complex_out-depths.txt', 'w')
+output.write(depthstring)
 output.close()      ## Mandatory closing
-depthsfile.close()  ## Mandatory closing
